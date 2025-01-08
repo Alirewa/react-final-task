575c93837307ac95f77a49430417afe301c692c9
Alireza <Alireza.pourgholam444@gmail.com>
2025-01-08 22:32:20 +0330

    s

--- src/components/body/AppLayout.jsx
+++ src/components/body/AppLayout.jsx
@@ -1,10 +1,13 @@
+import AddProductLayout from './features/AddProductLayout';
+import ProductList from './features/ProductList';
+
 function AppLayout() {
-  return (
-    <div className="flex gap-x-8 text-secondary-400">
-      <div>add product</div>
-      <div>product list</div>
-    </div>
-  );
+	return (
+		<div className="flex gap-x-8 container w-full justify-between items-start text-secondary-400">
+			<AddProductLayout />
+			<ProductList />
+		</div>
+	);
 }
 
 export default AppLayout;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,10 @@
 export default function Header() {
-  return (
-    <div className="p-2 w-full bg-secondary-700">
-      <div className="container flex justify-between items-center">
-        <h2 className="text-secondary-400 font-bold">Inventory App</h2>
-        <button className="bg-primary-800 text-white rounded-md px-4 py-2">
-          Reset All
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="p-2 w-full bg-secondary-700">
+			<div className="container flex justify-between items-center">
+				<h2 className="text-secondary-400 font-bold">Inventory App</h2>
+				<button className="btn--danger">Reset All</button>
+			</div>
+		</div>
+	);
 }
