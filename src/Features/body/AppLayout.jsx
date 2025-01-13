713512cb88a01ddedd42d20869a892f6165f3602
Alireza <Alireza.pourgholam444@gmail.com>
2025-01-13 13:10:13 +0330

    a

--- src/Features/body/AppLayout.jsx
+++ src/Features/body/AppLayout.jsx
@@ -1,37 +1,26 @@
-import { useState } from "react";
-import AddCategoryForm from "./handleApp/AddCategoryForm";
-import AddProductLayout from "./handleApp/AddProductLayout";
-import ProductList from "./handleApp/ProductList";
+import { useState } from 'react';
+import AddCategoryForm from './handleApp/AddCategoryForm';
+import AddProductLayout from './handleApp/AddProductLayout';
+import ProductList from './handleApp/ProductList';
 
 function AppLayout({ setProducts, products, options, setOptions }) {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div className="flex gap-8 mt-8 container h-screen w-full justify-start items-start text-secondary-400">
-      <div className="w-1/2 flex flex-col gap-y-4">
-        <div className="bg-secondary-700 p-4 rounded-md">
-          <p
-            className="text-secondary-400 rounded-md font-bold text-lg"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            Add New Category?
-          </p>
-          {isOpen ? (
-            <AddCategoryForm options={options} setOptions={setOptions} />
-          ) : (
-            ""
-          )}
-        </div>
-        <div className="flex items-start justify-start">
-          <AddProductLayout
-            setProducts={setProducts}
-            products={products}
-            options={options}
-          />
-        </div>
-      </div>
-      <ProductList products={products} />
-    </div>
-  );
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<div className="flex gap-8 mt-8 container h-screen w-full justify-start items-start text-secondary-400">
+			<div className="w-1/2 flex flex-col gap-y-4">
+				<div className="bg-secondary-700 p-4 rounded-md">
+					<p className="text-secondary-400 rounded-md font-bold text-lg" onClick={() => setIsOpen(!isOpen)}>
+						Add New Category?
+					</p>
+					{isOpen ? <AddCategoryForm options={options} setOptions={setOptions} /> : ''}
+				</div>
+				<div className="flex items-start justify-start">
+					<AddProductLayout setProducts={setProducts} products={products} options={options} />
+				</div>
+			</div>
+			<ProductList products={products} />
+		</div>
+	);
 }
 
 export default AppLayout;
